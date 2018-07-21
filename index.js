66d5089bd34fd5cd31fdcf2f2c97cc06fe8b0d69
Mehmet Toprak <mtoprak53@gmail.com>
2018-07-21 15:09:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+function getFirstSelector(selector) {
+  const el = document.querySelector(selector)
+  return el
+}
+
+function nestedTarget() {
+  //const ele = document.querySelector().querySelector()
+  const ele = document.querySelector("#nested .target")
+}
